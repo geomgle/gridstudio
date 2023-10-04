b513d0fccd9527cc362dd4bbdaa5eae6575756ba
geomgle <eunjin.sul@gmail.com>
2023-10-04 15:10:54 +0900

    _

--- grid-app/static/js/mapping.js
+++ grid-app/static/js/mapping.js
@@ -11,7 +11,21 @@ document.addEventListener("keydown", function (event) {
         break;
       case 51: // Ctrl + 3
         event.preventDefault(); // prevent browser default action
-        document.querySelector("textarea.xterm-helper-textarea").focus();
+        // check if the terminal tab is not the current tab
+        let terminalTab = document.querySelector(
+          'div.tab[data-tab="terminal"]',
+        );
+        if (!terminalTab.classList.contains("current")) {
+          // click the terminal tab
+          terminalTab.click();
+        }
+        // find the terminal textarea and focus it
+        let terminalTextarea = document.querySelector(
+          "textarea.xterm-helper-textarea",
+        );
+        if (terminalTextarea) {
+          terminalTextarea.focus();
+        }
         break;
       default:
         break;
